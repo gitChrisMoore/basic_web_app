62bb892aec334e6d146b61c7882f0cf32d2a22be
Chris <chrisdmoore06@gmail.com>
2016-02-17 14:30:30 -0500

    fixed initial round of testing

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,18 +1,49 @@
+/**
+ * routes.js
+ * 
+ * This file is responsable for declaring all of the routes
+ * in which the Express middleware framework is listening for
+ */
+
 'use strict';
 
-//var errors = require('./assets/errors');
-var mongoose = require('mongoose');
-var parser = require('./middleware/parser')
-var checkpathvalid = require('./middleware/checkpathvalid')
-var mongofind = require('./middleware/mongofind')
-var parserip = require('./middleware/parserip')
+/**
+* Global requirements
+*
+*/
 
+/**
+* NPM requirements
+*
+*/
+var mongoose = require('mongoose');
+
+/**
+* Local requirements
+*
+*/
+var parser = require('./middleware/v1/parser');
+var checkpathvalid = require('./middleware/v1/checkpathvalid');
+var parserip = require('./middleware/v1/parserip');
 var ver1 = '/api/v1';
+var filename = 'routes.js';
+var logger = require('./middleware/v1/loggerService');
+var filename = 'routes.js'
+var mongoService = require('./middleware/v1/mongoService')
+/**
+* Export all of the valid routes
+*
+* GET
+*
+* POST
+*
+*/
 
 module.exports = function(app) {
 
   app.get(ver1 + '/:database/:collection/:action', function (req, res, next) {
     // Check and see if the path has a model that exists
+    logger.info(filename, 'START: GET /:database/:collection/:action')
     checkpathvalid(req, res)
         .then(function (isPathValid) {
           // Use the parser function to determine what/if there needs to be a query
@@ -20,10 +51,11 @@ module.exports = function(app) {
         })
         .then(function (query) {
           // Query the mongo database with the query field
-          return mongofind(req, res, query)
+          return mongoService.find(req, res, query)
         })
         .then(function (result) {
           // return to the client a json result file from the query
+          logger.info(filename, 'SUCCESS: GET /:database/:collection/:action')
           res.status(200).json(result)
         })
         .catch(next);
@@ -31,6 +63,7 @@ module.exports = function(app) {
 
   app.get(ver1 + '/:database/:collection/', function (req, res, next) {
     // Check and see if the path has a model that exists
+    logger.info(filename, 'START: GET /:database/:collection/')
     checkpathvalid(req, res)
         .then(function (isPathValid) {
           // Use the parser function to determine what/if there needs to be a query
@@ -38,11 +71,52 @@ module.exports = function(app) {
         })
         .then(function (query) {
           // Query the mongo database with the query field
-          return mongofind(req, res, query)
+          return mongoService.find(req, res, query)
         })
         .then(function (result) {
           // return to the client a json result file from the query
+          logger.info(filename, 'SUCCESS: GET /:database/:collection/')
           res.status(200).json(result)
+        })
+        .catch(next);
+  });
+
+  app.post(ver1 + '/:database/:collection/', function (req, res, next) {
+    // Check and see if the path has a model that exists
+    logger.info(filename, 'START: POST /:database/:collection/')
+    checkpathvalid(req, res)
+        .then(function (isPathValid) {
+          // Use the parser function to determine what/if there needs to be a query
+          return parser(req, res)
+        })
+        .then(function (query) {
+          // Query the mongo database with the query field
+          return mongoService.create(req, res, query)
+        })
+        .then(function (result) {
+          // return to the client a json result file from the query
+          logger.info(filename, 'SUCCESS: POST /:database/:collection/')
+          res.status(202).json(result)
+        })
+        .catch(next);
+  });
+
+  app.patch(ver1 + '/:database/:collection/:id', function (req, res, next) {
+    // Check and see if the path has a model that exists
+    logger.info(filename, 'START: PATCH /:database/:collection/')
+    checkpathvalid(req, res)
+        .then(function (isPathValid) {
+          // Use the parser function to determine what/if there needs to be a query
+          return parser(req, res)
+        })
+        .then(function (query) {
+          // Query the mongo database with the query field
+          return mongoService.update(req, res, query)
+        })
+        .then(function (result) {
+          // return to the client a json result file from the query
+          logger.info(filename, 'SUCCESS: PATCH /:database/:collection/')
+          res.status(202).json(result)
         })
         .catch(next);
   });
--- server/test/routes.js
+++ server/test/routes.js
@@ -6,13 +6,58 @@ var agent = request.agent(app);
 
 //
 var ver1 = '/api/v1'
+var testID = ''
 
 
 
     // get users
-    describe('GET: ' + ver1 + '/amwayio/circuits/ 200 array', function() {
+    describe('GET: ' + ver1 + '/amwayio/circuits/ ', function() {
 
-      it('It should return a 200 OK message and a array', function(done) {
+      it('This should return an array of objects', function(done) {
+        agent
+        .get(ver1 + '/amwayio/circuits/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.should.have.property('status', 200)
+          res.body.should.be.a('array');
+          done();
+        });
+      });
+    
+    });
+
+
+    // get users
+    describe('Post: ' + ver1 + '/amwayio/circuits/ ', function() {
+
+      it('This should create a blank object and return the object _id', function(done) {
+        var object = {}
+        agent
+        .post(ver1 + '/amwayio/circuits/')
+        .send(object)
+        .expect(202)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.should.have.property('status', 202)
+          res.body.should.have.property('_id')
+          //res.body.should.be.a('array');
+          testID = res.body._id
+          done();
+        });
+      });
+    
+    });
+
+    // get users
+
+    var buildString = 'find?string={"_id":"' + testID  +'"}'
+
+    describe('GET: ' + ver1 + '/amwayio/circuits/' + buildString, function() {
+
+      it('This should return a single object from the previous get', function(done) {
         agent
         .get(ver1 + '/amwayio/circuits/')
         .expect(200)
